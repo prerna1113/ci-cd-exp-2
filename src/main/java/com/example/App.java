1954a8a4231435bef1aef1bd013454ed68f427b6
PrernaKumari <111172618+prerna1113@users.noreply.github.com>
2026-01-30 12:45:11 +0530

    Create App.java
    file added

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from CI/CD Maven Demo!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
